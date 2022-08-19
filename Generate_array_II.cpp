3ab995d2a0843652bc1cd468fe639194a7020851
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-08-19 16:06:47 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n;     cin>>n;     int a[n];     for(int i=0;i<n;i++)     {         cin>>a[i];     }     for(int i=0;i<n;i=i+2)     {         for(int j=0;j<a[i+1];j++)         {             cout<<a[i]<<" ";         }     } }

---
+++ Generate_array_II.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i=i+2)
+    {
+        for(int j=0;j<a[i+1];j++)
+        {
+            cout<<a[i]<<" ";
+        }
+    }
+}
